12647e9f946714d4bf8b0abfc5629d8164a8b8d9
Dqu1J <mcpeytwot5@gmail.com>
2022-03-30 14:27:05 +0300

    potion mechanic

--- src/main/java/com/mira/customizableenchants/mechanics/PotionMechanicType.java
+++ src/main/java/com/mira/customizableenchants/mechanics/PotionMechanicType.java
@@ -1,10 +1,26 @@
 package com.mira.customizableenchants.mechanics;
 
 import com.mira.customizableenchants.trigger.MechanicTrigger;
+import com.mira.customizableenchants.utils.MathUtils;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PotionMechanicType implements MechanicType {
     @Override
     public void execute(MechanicTrigger trigger) {
+        String[] args = trigger.mechanic().data().split(" ");
+        PotionEffectType effect = PotionEffectType.getByName(args[0]);
+        int duration = MathUtils.evaluateQuestion(args[1], trigger.level());
+        int amplifier = MathUtils.evaluateQuestion(args[2], trigger.level());
 
+        PotionEffect potion = new PotionEffect(effect, duration, amplifier);
+
+        if (args[3].equalsIgnoreCase("target") && trigger.entity().isPresent()) {
+            LivingEntity entity = (LivingEntity) trigger.entity().get();
+            entity.addPotionEffect(potion);
+        } else {
+            trigger.player().addPotionEffect(potion);
+        }
     }
 }
